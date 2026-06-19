5111c5a42945d322af3ccf3b0559b577ff4d3a1d
Yashasvini Bhanuraj <https://github.com/yashasvini2003>
2026-06-19 18:58:30 -0400

    Setup React Vite Tailwind Framer Motion

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        Tailwind Working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
